dec368aa64d5f362762f39a331a22d917d027a29
oscarcorres <ocorres@gmail.com>
2018-05-24 12:59:07 +0200

    Ejercicio 3

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="indice"),
+    path('<int:numero>', views.detail, name="detalle")
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Primer ejemplo de DJANGO</h1><b>"+
-                        "<h3>Esta es la página de Inicio (Índice)</h3><b>"+
-                        "<p>(Notes for building from source: <p>1) Don't do this on a standard Ubuntu installation, "
-                        "as you'll probably get a ""header and source version mismatch"" error, due to conflict"
-                        " between an already installed version and the newly installed one.</p> <p>2) If the make command "
-                        "seems to expect further input, just be patient, as the source can take a while to compile).</p></p>")
+    template = loader.get_template('home/index.html')
+    tituloPlantilla = 'Título de la Plantilla'
+    context = {'titulo': tituloPlantilla}
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request):
+    return None
