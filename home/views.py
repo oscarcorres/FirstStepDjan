819f8ddff2a6afebd3d6287b631b27cb748c4072
oscarcorres <ocorres@gmail.com>
2018-05-25 09:31:56 +0200

    Ejercicio 3

--- home/views.py
+++ home/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
+from random import randint
 
 # Create your views here.
 def index(request):
     template = loader.get_template('home/index.html')
     tituloPlantilla = 'Título de la Plantilla'
-    context = {'titulo': tituloPlantilla}
+    context = {'titulo': tituloPlantilla, 'numero': randint(1,49)}
     return HttpResponse(template.render(context, request))
 
 
-def detail(request):
-    return None
+def detail(request,numero):
+    template = loader.get_template('home/detail.html')
+    context = {'numero': numero}
+    return HttpResponse(template.render(context, request))
